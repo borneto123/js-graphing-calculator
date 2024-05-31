a7cfd06498a19a8090b591f7b321413a3a5b8b76
Borneto <bornaflinta2005@gmail.com>
2024-05-31 14:56:04 +0200

    Optimisations +added points slider +added new way of drawing function -step slider

--- js/dot.js
+++ js/dot.js
@@ -1,4 +1,5 @@
-import { origin, ctx} from "./main.js";
+import { origin, ctx, data, getFun} from "./main.js";
+import { Func } from "./function.js";
 export class Dot{
     constructor(data){
         this.width = data.width;
@@ -40,5 +41,50 @@ export class Dot{
         let dis = Math.sqrt(Math.pow(A.canvas_x-B.canvas_x,2)+Math.pow(A.canvas_y-B.canvas_y,2))
         //console.log(dis);
         return dis;
+    }
+
+    /**
+     * 
+     * @param {Array.<Dot>} dots 
+     */
+    static drawLineFromArray(dots){
+        let path = new Path2D();
+        path.moveTo(dots[0].canvas_x,dots[0].canvas_y);
+        for(let i=1; i<dots.length; i++){
+            path.lineTo(dots[i].canvas_x,dots[i].canvas_y);
+        }
+        //path.closePath();
+        ctx.lineWidth = 2;
+        ctx.lineCap = 'round'
+        ctx.lineJoin = 'round'
+        
+        ctx.stroke(path);
+        
+    }
+    /**
+     * 
+     * @param {Array.<Dot>} dots 
+     */
+    static drawCurveFromArray(dots, parX, parY){
+        let path = new Path2D();
+        
+        for(let i=1; i<dots.length; i++){
+            path.moveTo(dots[i-1].canvas_x,dots[i-1].canvas_y);
+            let cp = new Dot(data);
+            //let cpX = ((dots[i-1].x+dots[i].x)/2) - 0.75*dots[i-1].x - 0.25* dots[i].x ;
+            let cpX = ((dots[i-1].x+dots[i].x)/2)
+            let rezY = Func.racunFunkcija(getFun(),cpX);
+
+            let cpY = rezY - (dots[i-1].y+dots[i].y)/2 + (dots[i-1].y+dots[i].y)/2;
+            cp.setCoord(cpX,cpY);
+            path.quadraticCurveTo(cp.canvas_x,cp.canvas_y,dots[i].canvas_x,dots[i].canvas_y);
+        }
+        //.closePath();
+        ctx.lineWidth = 2;
+        ctx.lineCap = 'round'
+        ctx.lineJoin = 'round'
+        
+        ctx.stroke(path);
+        
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { Dot } from "./dot.js";
 import { Func } from "./function.js";
-const width = 1000;
-const height = 800;
+const width = 800;
+const height = 600;
 
 let c = document.getElementById("canv");
 /**@type {CanvasRenderingContext2D} */
@@ -13,7 +13,7 @@ export let origin = {
     x:width/2,
     y:height/2
 }
-let data = {
+export let data = {
     width :width,
     height : height,
     scale : 50,
@@ -23,20 +23,28 @@ let data = {
 let point = [];
 let range;
 let nov = true;
+
+let curve = true;
+
 let dif = parseInt(document.getElementById("dif").value)/100;
 
+let parmX = parseInt(document.getElementById("parmX").value)/1000;
+let parmY = parseInt(document.getElementById("parmY").value)/1000;
 
-range = parseInt(document.getElementById("range").value)/10000;
+
+range = parseInt(document.getElementById("range").value)*10;
 data.scale = parseInt(document.getElementById("scale").value);
 
-const getFun = () =>{
+export const getFun = () =>{
     nov = document.getElementById("good").checked ;
+    curve = document.getElementById("curve").checked ;
     //console.log(nov);
     return document.getElementById("ex").value;
 }
 
 const generatePoint= () =>{
     let interval = width/data.scale;
+    range = interval/range;
     for(let i=-interval/2; i<=interval/2; i+=range){
         let temp = new Dot(data);
         temp.setCoord(i,Func.racunFunkcija(getFun(),i));
@@ -54,20 +62,56 @@ const generatePoint= () =>{
     document.getElementById("points").innerHTML = `Rendered Points:${point.length}`
 }
 
+
+const randomGeneratePoint= () =>{
+    let interval = width/data.scale;
+    for(let i=-interval/2; i<=interval/2; i+=range){
+        let temp = new Dot(data);
+        temp.setCoord(i,Func.racunFunkcija(getFun(),i));
+        
+        let idx = point.length;
+        if(nov && idx >=2){
+            if(Dot.k(point[idx-1],temp) < dif){
+                continue;
+            }
+        }
+        point.push(temp);
+        range = getRandomNumber(range/1.5,range*1.5);
+    }
+    //console.log("Point:"+point.length);
+    document.getElementById("points").innerHTML = `Rendered Points:${point.length}`
+}
+
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 const draw = () =>{
+    
     for(let i=0;i<point.length-1; i++){
         Dot.DrawLine(point[i],point[i+1]);
     }
 }
 
 const sliderRefresh= () =>{
-    range = parseInt(document.getElementById("range").value)/10000;
+    range = parseInt(document.getElementById("range").value)*10;
     data.scale = parseInt(document.getElementById("scale").value);
     dif = parseInt(document.getElementById("dif").value)/100;
+    parmX = parseInt(document.getElementById("parmX").value)/1000;
+    parmY = parseInt(document.getElementById("parmY").value)/1000;
     ctx.reset();
     point = [];
     generatePoint();
-    draw();
+   //randomGeneratePoint();
+  //  draw();
+    console.log(curve)
+    if(curve)
+        Dot.drawCurveFromArray(point,parmX,parmY);
+    else
+        Dot.drawLineFromArray(point);
+    console.log(`ParmX:${parmX}`)
+    console.log(`ParmY:${parmY}`)
    // console.log(`Range:${range}`)
     //console.log(`Scale:${data.scale}`)
     //console.log(`Dif:${dif}`)
@@ -75,17 +119,22 @@ const sliderRefresh= () =>{
 }
 
 export const test = () =>{
-
+    ctx.translate(0.5, 0.5);
     let dot_A = new Dot(data);
     let dot_B = new Dot(data);
 
-    dot_A.setCoord(1,1);
+    dot_A.setCoord(0,0);
     dot_B.setCoord(1,2);
 
-   // Dot.DrawLine(dot_A,dot_B);
+    // Dot.DrawLine(dot_A,dot_B);
     generatePoint();
-    draw();
+    Dot.drawLineFromArray(point);
     console.log(point)
+
+    // ctx.beginPath();
+    // ctx.arc(origin.x, origin.y, 10, 0, 2 * Math.PI);
+    // ctx.stroke();
+
 }
 
 
@@ -96,4 +145,11 @@ document.getElementById("scale").addEventListener('input',sliderRefresh);
 document.getElementById("dif").addEventListener('input',sliderRefresh);
 document.getElementById("gumb").addEventListener('click',sliderRefresh);
 document.getElementById("good").addEventListener('input',sliderRefresh);
+
+
+document.getElementById("ex").addEventListener('input',sliderRefresh);
+
+document.getElementById("curve").addEventListener('input',sliderRefresh);
+document.getElementById("parmX").addEventListener('input',sliderRefresh);
+document.getElementById("parmY").addEventListener('input',sliderRefresh);
 test()
